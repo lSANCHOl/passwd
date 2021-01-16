62ee1f821dbf6538f2ae2614de29f69ec6e0aac4
lSANCHOl <60328198+lSANCHOl@users.noreply.github.com>
2021-01-16 18:13:41 +0000

    Update password_generator.py
    added ability to save list to file and added numbers option

--- password_generator.py
+++ password_generator.py
@@ -21,6 +21,7 @@ print("5: ", char_set5)
 print("6: ", char_set6)
 print("7: ", char_set7)
 print("8: ", char_set8)
+print("9: custom_set")
 
 choice = input("which option of characters do you want to include?: ")
 characters = ""
@@ -41,16 +42,52 @@ if choice == "7":
 	characters = char_set7
 if choice == "8":
 	characters = char_set8
+if choice == "9":
+   characters = input("input characters you want to use: ")
 
 length = len(characters)
 pass_len = int(input("how long for the password?: "))
 multiple = int(input("how many passwords should be created?: "))
 password = ""
+passwords = [ ]
+mylist = []
+while len(mylist) < multiple:
+	for j in range(multiple):
+		for i in range(pass_len):
+			ran_num = random.randint(0,length - 1)
+			ran_char = characters[ran_num]
+			password += ran_char
+		passwords.append(password)
+		password = ""
+		mylist = passwords
+		mylist = list(dict.fromkeys(mylist))
+	
 
-for j in range(multiple):
-	for i in range(pass_len):
-		ran_num = random.randint(0,length - 1)
-		ran_char = characters[ran_num]
-		password += ran_char
-	print("[PASSWORD][+] ",password," [+]")
-	password = ""
+output = input("print resuts or store in text file? [p/s]: ")
+appearance = input("show numbers on side? [y/n]: ")
+
+if output == "p":
+    if appearance == "y":
+        for i in range(len(mylist)):
+            print(i+1,".",mylist[i-1])
+    if appearance == "n":
+        for i in range(len(mylist)):
+            print(mylist[i-1])
+
+if output == "s":
+    f = open("passwd.txt","w+")
+    if appearance == "y":
+        print("[+]Passwords are stored as \'passwd.txt\' in current dirctory[+]")
+        for i in range(len(mylist)):
+            i_str = i+1
+            f.write(str(i+1))
+            f.write(".")
+            f.write(mylist[i-1])
+            f.write('\n')
+            f.close
+        if appearance == "n":
+            print("[+]Passwords are stored as \'passwd.txt\' in current dirctory[+]")
+            for i in range(len(mylist)):
+                f.write(mylist[i-1])
+                f.write('\n')
+                f.close
